7d16c6cf9f2739042a589777e3c8d6c2ce0a0ccf
沈福利 <fuli.shen@9icaishi.net>
2017-07-10 14:21:53 +0800

    update comments

--- word2vec_gensim_package/gensim_word2vec_Example/test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-sentence = "我 爱 北京 天安门"
-words = sentence.split()
-print words,type(words)
-for word in words:
-    print word,
-
-
-
---
+++ word2vec_jvm_package/Chinese-text-segmentation/README.md
@@ -0,0 +1,4 @@
+#tf-idf/textrank + ansj text seg
+reference:  
+https://github.com/NLPchina/ansj_seg  
+https://github.com/hankcs/TextRank
